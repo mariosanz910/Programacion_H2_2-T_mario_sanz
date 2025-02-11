29af1e5e4f1aec9d42e0d44b6a9cd14d8724325d
mariosanz910 <mario.sanzvillarejo24@campusfp.es>
2025-02-11 13:56:28 +0100

    Redirección al cerrar sesión
    Redirección al cerrar sesión

--- Programacion_H2_2ºT_MarioSanz/vista/controlsesion.php
+++ Programacion_H2_2ºT_MarioSanz/vista/controlsesion.php
@@ -73,7 +73,7 @@ $Usuarios = $controller->listartareas();
 <div class="text-center mt-3">
 <a href="registrar_tarea.php?id_usuario=<?= urlencode($id_usuario) ?>" class="btn btn-success">Agregar tareas</a>
 
-<a href="inicio_sesion.php" class="btn btn-danger">Cerrar sesión</a>
+<a href="errorsesion.php" class="btn btn-danger">Cerrar sesión</a>
 
 </div>
 
