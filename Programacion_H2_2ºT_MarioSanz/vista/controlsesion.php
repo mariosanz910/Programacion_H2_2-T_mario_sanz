926afba8ec7e355171e934cdf95dca0b72988763
mariosanz910 <mario.sanzvillarejo24@campusfp.es>
2025-02-14 12:54:26 +0100

    Update controlsesion.php

--- Programacion_H2_2ºT_MarioSanz/vista/controlsesion.php
+++ Programacion_H2_2ºT_MarioSanz/vista/controlsesion.php
@@ -35,7 +35,7 @@ $Usuarios = $controller->listartareas();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listado de Usuarios</title>
+    <title>Listado de Tareas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
